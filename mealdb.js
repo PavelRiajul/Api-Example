ceb3bc5543d67535f19254a388348a27a90422c9
Riajul Islam <riajulpislam@gmail.com>
2022-04-20 18:00:16 +0600

    api

--- mealdb.js
+++ mealdb.js
@@ -12,7 +12,26 @@ const searchFood = () => {
 const displaySerachResult = meals => {
     const searchResult = document.getElementById('search-result')
     meals.forEach(meal => {
-        console.log(meal)
+       // console.log(meal)
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div onclick="loadmialDeatil(${meal.idMeal})" class="card h-100">
+        <img src="${meal.strMealThumb} " class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal} </h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+        `
+        searchResult.appendChild(div)
+        
     })
+}
+const loadmialDeatil = mealId => {
+const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}` 
+fetch(url)
+.then(res => res.json())
+.then(data => console.log(data.meals) )
 }
 
