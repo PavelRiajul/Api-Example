8a4ff0f86c683c0ba71a1bacae62aeba4ee02ed7
Riajul Islam <riajulpislam@gmail.com>
2022-04-20 16:03:51 +0600

    Api Country

--- countries.js
+++ countries.js
@@ -10,15 +10,30 @@ const displayCountries = countries => {
 //}
 const countriesDiv = document.getElementById('countries')
 countries.forEach(country => {
-    console.log(country)
     const div = document.createElement('div')
     div.classList.add('country')
-    const h3 = document.createElement('h3')
-    h3.innerText = country.name;
-    div.appendChild(h3)
-    const p = document.createElement('p')
-    p.innerText = country.capital
-    div.appendChild(p)
+    div.innerHTML =`
+    <h3>${country.name}</h3>
+    <p>${country.capital} </p>
+    <button onclick="loadCountryByNmae('${country.name}')">Details</button>
+    `
     countriesDiv.appendChild(div)
 })
 }
+const loadCountryByNmae = name => {
+    const url = `https://restcountries.com/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country => {
+    const countriesDiv = document.getElementById('country-detail')
+    countriesDiv.innerHTML =`
+   <h5>${country.name} </h5>
+   <p>popolation:${country.population} </p>
+   <img src="${country.flag}">
+    `
+}
+
+    
+   
